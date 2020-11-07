e29eb35eeaf25ee1c017bb2ac44d4c9d0082d72a
tsamantanis <philip.tsamantanis@icloud.com>
2020-11-07 12:19:34 -0800

    Replace canvas with SVG

--- main.js
+++ main.js
@@ -1,43 +1,3 @@
-const canvas = document.getElementById('HeaderCanvas');
-const ctx = canvas.getContext('2d');
-
-const arcRadius = 150;
-const arcWidth = 4;
-const primaryColor = '#FFA100';
-const secondaryColor = '#2EC4B6';
-const colors = ['#FFA100', '#2EC4B6', '#FFBF69', '#CBF3F0'];
-const radians = [Math.PI / 2, 3 * Math.PI / 4, Math.PI, 5 * Math.PI / 4, 3 * Math.PI / 2, 7 * Math.PI / 4, 2 * Math.PI];
-const arcPositions = generateArcPositionPairs();
-
-let x = canvas.width / 2;
-let y = canvas.height / 2;
-let dy = radians[getRandomInt(radians.length)];
-let dx = radians[getRandomInt(radians.length)];
-let angle = Math.atan2(dy, dx);
-let percent = 0;
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-
-function generateArcPositionPairs() {
-    let arcPositionPairs = [];
-    for (let k = arcRadius; k < canvas.width; k += 5 * arcWidth) {
-        arcPositionPairs.push({x: radians[getRandomInt(radians.length)], y: radians[getRandomInt(radians.length)], arcRadius: k})
-    }
-    return arcPositionPairs
-}
-
-function drawArcs() {
-    arcPositions.forEach((position) => {
-        ctx.beginPath();
-        ctx.arc(x, y, position.arcRadius, position.x + percent, position.y + percent);
-        ctx.strokeStyle = colors[getRandomInt(colors.length)];
-        ctx.lineWidth = arcWidth;
-        ctx.stroke();
-    })
-}
-
 window.addEventListener("scroll", function(e) {
     let paths = [
         document.getElementById('scrollDash'),
@@ -68,20 +28,15 @@ window.addEventListener("scroll", function(e) {
 
     projects.forEach((project) => {
         let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-        // let scrollPosition = (document.documentElement.scrollTop + document.body.scrollTop) / 100;
-        console.log(project);
-        console.log("Scroll position: " + project.getBoundingClientRect().top)
         if (project.getBoundingClientRect().top > 1200) {
             project.style.opacity = 0;
-        } else if (project.getBoundingClientRect().top < 1200 && project.getBoundingClientRect().top >= 100) {
-            project.style.opacity = 1 - (project.getBoundingClientRect().top / 350)
-        } else if (project.getBoundingClientRect().top < 100 && project.getBoundingClientRect().top >= -100) {
+        } else if (project.getBoundingClientRect().top < 1800 && project.getBoundingClientRect().top >= 300) {
+            project.style.opacity = 1 - (project.getBoundingClientRect().top / 1000)
+        } else if (project.getBoundingClientRect().top < 300 && project.getBoundingClientRect().top >= -100) {
             project.style.opacity = 1
         } else if (project.getBoundingClientRect().top < 0) {
             project.style.opacity = 1 - (- project.getBoundingClientRect().top / 350)
         }
     })
 });
-
-drawArcs()
 
